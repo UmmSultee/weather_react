1eff446ba505949a67b1b6f603b232491350801f
Hauwa Haruna <106313850+UmmSultee@users.noreply.github.com>
2024-05-24 19:59:59 +0100

    Touches

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Weather App</p>
+      <h1>Weather App</h1>
 
-        <Weather />
-      </header>
+      <Weather />
+      <p>
+        This project is hosted on{" "}
+        <a href="https://init-weather-react.netlify.app/" target="_blank">
+          Netlify
+        </a>
+        and is
+        <a href="https://github.com/UmmSultee/weather_react" target="_blank">
+          {" "}
+          open source
+        </a>
+      </p>
     </div>
   );
 }
